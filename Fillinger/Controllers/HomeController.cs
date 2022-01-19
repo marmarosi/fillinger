193cfa221fbe1ce15e0c446980e46f8c9a65add9
Mármarosi József <marmarosi@logikum.hu>
2022-01-19 17:01:38 +0100

    Add create ember

--- Fillinger/Controllers/HomeController.cs
+++ Fillinger/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Fillinger.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Fillinger.Controllers
@@ -31,6 +32,35 @@ namespace Fillinger.Controllers
         public IActionResult Emberek()
         {
             return View(_context.Emberek);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("Nev,Nem,SzuletesiDatum")] Ember ember)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(ember);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Emberek));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(ember);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
